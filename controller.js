4317163b67b35d9d43dd2b99a8cda734ee194c3c
ekokurniadi <ekokurniadi.02@gmail.com>
2020-11-17 15:48:05 +0700

    berhasil menampilkan data berdasarkan id

--- controller.js
+++ controller.js
@@ -17,3 +17,15 @@ exports.tampilSemuaMahasiswa = function (req, res) {
         }
     })
 }
+
+// menampilkan data mahasiswa berdasarkan id
+exports.getById = function (req, res) {
+    let id = req.params.id;
+    connection.query('select * from mahasiswa where id_mahasiswa = ?', [id], function (error, rows, fields) {
+        if (error) {
+            console.log(error)
+        } else {
+            response.ok(rows, res)
+        }
+    });
+}
