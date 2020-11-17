46eb871b06f91573bfb1a831b806846c580dbe79
ekokurniadi <ekokurniadi.02@gmail.com>
2020-11-17 15:36:55 +0700

    berhasil menampilkan data mahasiswa

--- controller.js
+++ controller.js
@@ -6,3 +6,14 @@ var connection = require('./koneksi');
 exports.index = function (req, res) {
     response.ok("Aplikasi rest api ku berjalan", res)
 };
+
+// menampilkan data mahasiswa
+exports.tampilSemuaMahasiswa = function (req, res) {
+    connection.query("select * from mahasiswa", function (error, rows, fields) {
+        if (error) {
+            console.log(error)
+        } else {
+            response.ok(rows, res)
+        }
+    })
+}
