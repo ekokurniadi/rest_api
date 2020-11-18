3090138f5d7e05a212e09c83233d208ebd94052b
ekokurniadi <ekokurniadi.02@gmail.com>
2020-11-18 13:11:01 +0700

    berhasil register user

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+var morgan = require('morgan')
 const app = express();
 
 // parse application json
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(morgan('dev'))
+
+// daftarkan menu routes dari index
+app.use('/auth', require('./middleware'))
 
 // panggil routes
 var routes = require('./routes');
